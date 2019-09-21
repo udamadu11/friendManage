67e255eb7abc0905968c1311e6a4bf9fb8643eb9
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-21 14:15:01 +0530

    update

---
+++ php/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+  <form  style="margin-top: 200px;margin-left: 300px;">
+  <fieldset>
+    <div class="form-group row">
+    </div>
+    <div class="form-group" style="width: 400px;">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group" style="width: 400px;">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+   
+    </fieldset>
+    <button type="submit" class="btn btn-primary" style="width: 400px;">Submit</button>
+  </fieldset>
+</form>
+</div>
+</body>
+</html>
