4da699fa540a8916f47db57438c1a279ff8a2303
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-29 12:32:43 +0530

    0.14

--- php/dashboard.php
+++ php/dashboard.php
@@ -4,6 +4,7 @@
 	<title>Dashbord</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 <body>
  	<h4 style="text-align: center;margin-top: 20px;" class="display-4">Welcome to Dashbord</h4>
@@ -29,13 +30,57 @@
     
   </div>
 </div>
-<div class="card text-white bg-success mb-3" style="max-width: 20rem;margin-right: 8px;">
+<div class="card text-white bg-danger mb-3" style="max-width: 20rem;margin-right: 8px;">
   <div class="card-header">Drug</div>
   <div class="card-body">
     <h4 class="card-title">Success card</h4>
     
   </div>
 </div>
+<div class="card text-white bg-dark mb-3" style="max-width: 20rem;margin-right: 8px;">
+  <div class="card-header">Drug</div>
+  <div class="card-body">
+    <h4 class="card-title">Success card</h4>
+    
+  </div>
+</div>
+<div class="card text-white bg-info mb-3" style="max-width: 20rem;margin-right: 8px;">
+  <div class="card-header">Drug</div>
+  <div class="card-body">
+    <h4 class="card-title">Success card</h4>
+    
+  </div>
+</div>
+
+</div>
+<div class="row">
+	
+	<script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 600],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+<div id="columnchart_material" style="width: 800px; height: 500px;margin-left: 200px; margin-top: 100px;"></div>
 </div>
 </body>
 </html>
