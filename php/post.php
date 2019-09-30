fe969871ce4c96605e90a9c819e161927d065ea7
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-30 15:05:57 +0530

    0.16

--- php/post.php
+++ php/post.php
@@ -6,50 +6,72 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<div class="container" style="margin-top: 100px;">
-    <button class="btn btn-dark btn-lg btn-block" name="submit" style="margin-bottom: 30px;">Post</button>
-<form method="post">
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label>Title</label>
-      <input type="text" class="form-control" name="title" required> 
-    </div>
-    <div class="form-group col-md-4">
-      <label>Email</label>
-      <input type="text" class="form-control" name="email" required>
-    </div>
-    <div class="form-group col-md-2">
-      <label>Contact</label>
-      <input type="number" class="form-control" name="contact" required>
-    </div>
-  </div>
-   <div class="form-group">
-    <label>Post Image</label>
-    	<div class="custom-file">
-  			<input type="file" class="custom-file-input">
-  			<label class="custom-file-label" for="customFile">Choose file</label>
-		</div>
- 	 </div>
+  <div class="container" style="margin-top: 100px;">
+    <form method="post" enctype="multipart/form-data"> 
 
-  <div class="form-row">
-    <div class="form-group col-md-12">
-      <lable>Content</lable>
-    	<textarea name="content" id="editor" class="form-control">
-        &lt;p&gt;This is some sample content.&lt;/p&gt;
-    	</textarea>
-	   	 <script>
-	        ClassicEditor
-	            .create( document.querySelector( '#editor' ) )
-	            .catch( error => {
-	                console.error( error );
-	            } );
-	    </script>
-    </div>
-  </div>
-  
-  
-  <button type="submit" class="btn btn-dark btn-lg btn-block" name="submit">Add User</button>
-</form>
+          <div class="row">
+              <div class="form-group col-md-6">
+                   <label>Title</label>
+                   <input type="text" class="form-control"  name="title" required> 
+             </div>
+             <div class="form-group col-md-4">
+                   <label>Email</label>
+                   <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="form-group col-md-2">
+                   <label>Contact</label>
+                   <input type="number" class="form-control" name="contact" required>
+            </div>
+        </div>
+
+          <div>
+            <div class="form-group">
+
+              <div class="custom-file">
+                   <input type="file" class="custom-file-input" id="customFile" name="image">
+                <label class="custom-file-label" for="customFile">Choose file</label>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-14">
+            <label>Content</label>
+              <textarea  id="editor" name="editor">&lt;p&gt;This is some sample content.&lt;/p&gt;</textarea>
+              <script>
+            ClassicEditor
+              .create( document.querySelector( '#editor' ) )
+              .catch( error => {
+                  console.error( error );
+              } );
+             </script>
+          </div>
+          <div class="form-group col-md-14">
+            <input type="submit" name="submit" value="Post" class="btn btn-primary btn-lg btn-block" style="margin-top: 40px;">
+          </div>
+      
+    </form>
 </div>
 </body>
 </html>
+<?php
+include('include/connection.php');
+if (isset($_POST['submit'])) {
+
+  $title = $_POST['title'];
+  $email = $_POST['email'];
+  $contact = $_POST['contact'];
+
+  $image = $_FILES['image']['name'];
+  $imagetmp = $_FILES['image']['tmp_name'];
+  $dir = '../image/';
+  move_uploaded_file($imagetmp, $dir.$image);
+  
+
+  $editer = $_POST['editor'];
+
+  $sql = "INSERT INTO post(title,email,contact,image,content)VALUES('$title','$email','$contact','$image','$editer')";
+  $result = mysqli_query($con,$sql);
+}
+
+?>
