95ed95ff548b335bccb6e855fd8ab5570d24523c
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-30 18:25:20 +0530

    0.18

--- php/post.php
+++ php/post.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-  <div class="container" style="margin-top:100px;">
+  <div class="container" style="margin-top:100px">
 
     <input type="submit" value="Add Post" class="btn btn-dark btn-lg btn-block" style="margin-bottom: 40px;">
 
